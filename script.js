cbc5016b8e959c5e9645745b269a3c91d52bdc5c
dul-media <156544527+dul-media@users.noreply.github.com>
2024-01-14 05:11:13 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,132 @@
+const workIdList = ['newborn', 'babbling', 'space', 'vivid', 'somewhere', 'ardmz', 'kingdom', 'club', 'library', 'meta', 'orang']
+
+$(document).ready(function () {
+    // for (step = 0; step < workIdList.length; step++){
+    //     $("#work-" + workIdList[step] + " .button").click(function () {
+    //         if ($("#work-" + workIdList[step] + " .work-main").css('display') == 'none') {
+    //             $("#work-" + workIdList[step] + " .work-main").show(500);
+    //         } else {
+    //             $("#work-" + workIdList[step] + " .work-main").hide(500);
+    //         }
+    //     })
+    // }
+    $("#work-newborn .button").click(function () {
+        if ($('#work-newborn .hidden').css('display') == 'none') {
+            $("#work-newborn .hidden").show(500);
+        } else {
+            $("#work-newborn .hidden").hide(500);
+        }
+    })
+    $("#work-babbling .button").click(function () {
+        if ($('#work-babbling .hidden').css('display') == 'none') {
+            $("#work-babbling .hidden").show(500);
+        } else {
+            $("#work-babbling .hidden").hide(500);
+        }
+    })
+    $("#work-space .button").click(function () {
+        if ($('#work-space .hidden').css('display') == 'none') {
+            $("#work-space .hidden").show(500);
+        } else {
+            $("#work-space .hidden").hide(500);
+        }
+    })
+    $("#work-vivid .button").click(function () {
+        if ($('#work-vivid .hidden').css('display') == 'none') {
+            $("#work-vivid .hidden").show(500);
+        } else {
+            $("#work-vivid .hidden").hide(500);
+        }
+    })
+    $("#work-somewhere .button").click(function () {
+        if ($('#work-somewhere .hidden').css('display') == 'none') {
+            $("#work-somewhere .hidden").show(500);
+        } else {
+            $("#work-somewhere .hidden").hide(500);
+        }
+    })
+    $("#work-ardmz .button").click(function () {
+        if ($('#work-ardmz .hidden').css('display') == 'none') {
+            $("#work-ardmz .hidden").show(500);
+        } else {
+            $("#work-ardmz .hidden").hide(500);
+        }
+    })
+    $("#work-chusu .button").click(function () {
+        if ($('#work-chusu .hidden').css('display') == 'none') {
+            $("#work-chusu .hidden").show(500);
+        } else {
+            $("#work-chusu .hidden").hide(500);
+        }
+    })
+    $("#work-chopeong .button").click(function () {
+        if ($('#work-chopeong .hidden').css('display') == 'none') {
+            $("#work-chopeong .hidden").show(500);
+        } else {
+            $("#work-chopeong .hidden").hide(500);
+        }
+    })
+    $("#work-kingdom .button").click(function () {
+        if ($('#work-kingdom .hidden').css('display') == 'none') {
+            $("#work-kingdom .hidden").show(500);
+        } else {
+            $("#work-kingdom .hidden").hide(500);
+        }
+    })
+    $("#work-club .button").click(function () {
+        if ($('#work-club .hidden').css('display') == 'none') {
+            $("#work-club .hidden").show(500);
+        } else {
+            $("#work-club .hidden").hide(500);
+        }
+    })
+    $("#work-library .button").click(function () {
+        if ($('#work-library .hidden').css('display') == 'none') {
+            $("#work-library .hidden").show(500);
+        } else {
+            $("#work-library .hidden").hide(500);
+        }
+    })
+    $("#work-meta .button").click(function () {
+        if ($('#work-meta .hidden').css('display') == 'none') {
+            $("#work-meta .hidden").show(500);
+        } else {
+            $("#work-meta .hidden").hide(500);
+        }
+    })
+    $("#work-orang .button").click(function () {
+        if ($('#work-orang .hidden').css('display') == 'none') {
+            $("#work-orang .hidden").show(500);
+        } else {
+            $("#work-orang .hidden").hide(500);
+        }
+    })
+    $("#movie-space .button").click(function () {
+        if ($('#movie-space .hidden').css('display') == 'none') {
+            $("#movie-space .hidden").show(500);
+        } else {
+            $("#movie-space .hidden").hide(500);
+        }
+    })
+    $("#movie-heritage .button").click(function () {
+        if ($('#movie-heritage .hidden').css('display') == 'none') {
+            $("#movie-heritage .hidden").show(500);
+        } else {
+            $("#movie-heritage .hidden").hide(500);
+        }
+    })
+    $("#movie-crack .button").click(function () {
+        if ($('#movie-crack .hidden').css('display') == 'none') {
+            $("#movie-crack .hidden").show(500);
+        } else {
+            $("#movie-crack .hidden").hide(500);
+        }
+    })
+    $("#movie-align .button").click(function () {
+        if ($('#movie-align .hidden').css('display') == 'none') {
+            $("#movie-align .hidden").show(500);
+        } else {
+            $("#movie-align .hidden").hide(500);
+        }
+    })
+})
